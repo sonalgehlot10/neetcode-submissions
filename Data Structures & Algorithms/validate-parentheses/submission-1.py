198971384332f101dd34c2e7baa199ea6871a8d2
Sonal Gehlot <sonalgehlot10@gmail.com>
2026-09-05 17:02:39 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closePToOpenP = {
+            '}':'{',
+            ']':'[',
+            ')':'('
+        }
+
+        for c in s:
+            if c not in closePToOpenP:
+                stack.append(c)
+            else:
+                if stack and stack[-1]==closePToOpenP[c]:
+                    stack.pop()
+                else:
+                    return False
+        
+        if not stack:
+            return True
+        return False
+
+# Time: O(n)
+# Space: O(n)
