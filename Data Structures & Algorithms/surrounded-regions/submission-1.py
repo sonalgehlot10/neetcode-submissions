176b8f0f5ce8b63e17361992600fe90e0857d935
Sonal Gehlot <sonalgehlot10@gmail.com>
2026-09-23 12:30:08 -0700

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS = len(board)
+        COLS = len(board[0])
+
+        def safe(r,c):
+            #CORNER 'O' SO CAN'T BE SURROUNDED
+            if (r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != 'O'):
+                return
+            board[r][c] = 'S'
+            safe(r+1, c)
+            safe(r-1, c)
+            safe(r, c+1)
+            safe(r, c-1)
+
+        for c in range(COLS):
+            safe(0, c)
+            safe(ROWS-1, c)
+
+        for r in range(ROWS):
+            safe(r, 0)
+            safe(r, COLS-1)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'S':
+                    board[r][c] = 'O'
+        
+# Time: O(m * n)
+# Space: O(m * n)
